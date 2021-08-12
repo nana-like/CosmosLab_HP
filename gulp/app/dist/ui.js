2b100b0eba6697a6301caf5968f035e6df2e038c
nana <nykim@nykim.net>
2021-08-12 20:33:15 +0900

    gulp 세팅

---
+++ gulp/app/dist/ui.js
@@ -0,0 +1,9 @@
+/**
+ * -----------------------------------------------
+ * Project: COSMOS LAB HOMEPAGE
+ * Author: Nana <nykim@nykim.net>
+ * Last Modified: 2021-08-12 20:31:43
+ * -----------------------------------------------
+ */
+
+console.log("ui.js")
---
+++ gulp/gulpfile.js
@@ -0,0 +1,132 @@
+global.project = require('./package.json');
+
+const gulp = require('gulp');
+const path = require('path');
+const del = require("del");
+const sass = require('gulp-sass')(require('dart-sass'));
+const browserSync = require("browser-sync");
+const fileinclude = require("gulp-file-include");
+const headerComment = require('gulp-header-comment');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const minify = require('gulp-minify');
+
+const paths = project.paths;
+const files = {
+  html: 'views/**/*.html',
+  css: 'styles/**/*.scss',
+  js: 'scripts/**/*.js'
+};
+
+const comment = require('./config/headerComment');
+const isBuildMode = false;
+
+const html = () => {
+  return gulp
+    .src([files.html, '!views/**/_*.*'], {
+      cwd: path.resolve(__dirname, paths.src)
+    })
+    .pipe(fileinclude({
+      prefix: "@@",
+      basepath: "@file",
+      context: {
+        "id": ""
+      },
+    }))
+    .pipe(gulp.dest(path.resolve(__dirname, paths.dist)))
+    .pipe(browserSync.stream());
+};
+
+var styleSheet = () => {
+  const opts = {
+    header: comment(project),
+    sass: {
+      outputStyle: 'expanded'
+    }
+  };
+
+  if (isBuildMode) {
+    opts.sass.outputStyle = 'compressed';
+  }
+
+  let stream = gulp
+    .src(files.css, {
+      cwd: path.resolve(__dirname, paths.src),
+      since: gulp.lastRun(styleSheet)
+    })
+    .pipe(sourcemaps.init())
+    .pipe(sass(opts.sass)).on('error', sass.logError)
+    .pipe(autoprefixer());
+
+    if (!isBuildMode) {
+      stream = stream.pipe(sourcemaps.write());
+    }
+
+  stream = stream.pipe(headerComment(opts.header))
+    .pipe(gulp.dest(path.resolve(__dirname, paths.dist)))
+    .pipe(browserSync.stream());
+
+  return stream;
+};
+
+
+var scripts = () => {
+  var opts = {
+    header: comment(project)
+  };
+  return gulp
+    .src(files.js, {
+      cwd: path.resolve(__dirname, paths.src),
+      // since: gulp.lastRun(scripts)
+    })
+    .pipe(minify())
+    .pipe(headerComment(opts.header))
+    .pipe(gulp.dest(path.resolve(__dirname, paths.dist)))
+    .pipe(browserSync.stream());
+}
+
+const sync = () => {
+  browserSync.init({
+    port: project.port,
+    server: {
+      baseDir: path.resolve(__dirname, paths.dist)
+    }
+  });
+};
+
+const watch = () => {
+  gulp.watch(path.resolve(__dirname, paths.src, files.html), html);
+  gulp.watch(path.resolve(__dirname, paths.src, files.css), styleSheet);
+  gulp.watch(path.resolve(__dirname, paths.src, files.js), scripts);
+}
+
+const clean = () => {
+  const opts = {
+    force: true
+  };
+
+  return new Promise((resolve, reject) => {
+    del(paths.dist, opts);
+    resolve();
+  });
+}
+
+const tasks = gulp.series(
+  // clean,
+  gulp.parallel(
+    sync,
+    html,
+    styleSheet,
+    scripts,
+    watch
+  )
+);
+
+gulp.task('default', async function () {
+  tasks();
+  console.log(" >>>> 😎 나나는 쩐다 😎 <<<<");
+});
+
+gulp.task('build', async function () {
+  console.log("🛠 빌드하고 있습니다.");
+})
